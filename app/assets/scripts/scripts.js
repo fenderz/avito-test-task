5fa49d718a3036a02212281472bc5cfba22ad8d1
Rinat Khafiyatullin <rkhafiyatullin@avito.ru>
2016-11-28 09:14:53 +0300

    Add animations

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -17,41 +17,57 @@ var indexLeft = 0;
 var indexRight = 0;
 var vol = 0;
 var arrLength = arr.length;
+var diff = {};
+function main() {
 
-while (indexLeft < arrLength) {
-    indexRight = indexLeft + 1;
-    maxIndex = 0;
 
-    // Ищем столбец с меньшим объемом
-    // либо упираемся в конец
-    while (arr[indexLeft] > arr[indexRight] && indexRight < arrLength) {
-        buffer.push(indexRight);
-        // Запоминаем индекс максимального элемента
-        // для случая если добрались до конца массива
-        // Индекс массива buffer
-        if (arr[buffer[maxIndex]] < arr[indexRight]) {
-            maxIndex = indexRight - indexLeft - 1;
+    while (indexLeft < arrLength) {
+        indexRight = indexLeft + 1;
+        maxIndex = 0;
+
+        // Ищем столбец с меньшим объемом
+        // либо упираемся в конец
+        while (arr[indexLeft] > arr[indexRight] && indexRight < arrLength) {
+            buffer.push(indexRight);
+            // Запоминаем индекс максимального элемента
+            // для случая если добрались до конца массива
+            // Индекс массива buffer
+            if (arr[buffer[maxIndex]] < arr[indexRight]) {
+                maxIndex = indexRight - indexLeft - 1;
+            }
+            indexRight++;
         }
-        indexRight++;
+
+        // Если добрались до конца, то
+        // отрезаем буфер до максимального элемента
+        if (arrLength === indexRight) {
+            indexRight = buffer[maxIndex];
+            buffer = buffer.slice(0, maxIndex);
+        }
+
+        buildDiff(arr, buffer, Math.min(arr[indexLeft], arr[indexRight]));
+        vol += calcVolume(arr, buffer, Math.min(arr[indexLeft], arr[indexRight]));
+        drowned = drowned.concat(buffer);
+        buffer = [];
+
+        // Продолжаем цикл с правой границы
+        // затопленных столбцов
+        indexLeft = indexRight;
     }
 
-    // Если добрались до конца, то
-    // отрезаем буфер до максимального элемента
-    if (arrLength === indexRight) {
-        indexRight = buffer[maxIndex];
-        buffer = buffer.slice(0, maxIndex);
-    }
+    arr.forEach(function (item, index) {
+        if (drowned.indexOf(index) !== -1) {
+            className = DEFAULT_CLASS + ' ' + FLOODED_CLASS;
+            drownedHeight =  diff[index];
+        } else {
+            drownedHeight = 0;
+            className = DEFAULT_CLASS;
+        }
+        containerNode.appendChild(createItem('li', className, item, drownedHeight));
+    });
 
-    vol += calcVolume(arr, buffer, Math.min(arr[indexLeft], arr[indexRight]));
-
-    drowned = drowned.concat(buffer);
-    buffer = [];
-
-    // Продолжаем цикл с правой границы
-    // затопленных столбцов
-    indexLeft = indexRight;
+    volumeNode.textContent = 'Volume: ' + vol;
 }
-
 function calcVolume(items, indexes, max) {
     var vol = 0;
 
@@ -62,21 +78,30 @@ function calcVolume(items, indexes, max) {
     return vol;
 }
 
-function createItem(tag, name, text) {
+function buildDiff(items, indexes, max) {
+
+    indexes.forEach(function (index) {
+        diff[index] = max - items[index];
+    });
+
+    return diff;
+}
+//console.log(diff);
+function createItem(tag, name, text, height) {
     var element = document.createElement(tag);
+    var span = document.createElement('span');
+    span.style.height = height * 25 + 'px';
+    span.className = 'list__item-submerged';
     element.className = name;
     element.textContent = text;
     element.style.height = text * 25 + 'px';
+    element.appendChild(span);
     return element
 }
+var drownedHeight;
 
-arr.forEach(function (item, index) {
-    if (drowned.indexOf(index) !== -1) {
-        className = DEFAULT_CLASS + ' ' + FLOODED_CLASS;
-    } else {
-        className = DEFAULT_CLASS;
-    }
-    containerNode.appendChild(createItem('li', className, item));
+
+containerNode.addEventListener('click', function () {
+    document.body.classList.remove('hide-drowned');
 });
-
-volumeNode.textContent = 'Volume: ' + vol;
+main();
